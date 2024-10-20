71cea45b423f673a03e23cffdee77225717ba2bb
İsmail ATAK <ismailatakofficial@gmail.com>
2024-10-20 19:13:31 +0300

    New currentVersion

--- version.go
+++ version.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-const currentVersion = "v0.0.7"
+const currentVersion = "v0.0.8"
 const repoURL = "https://api.github.com/repos/ismailatak/navicat-premium-trial-reset-go/releases/latest"
 
 func printVersion() {
